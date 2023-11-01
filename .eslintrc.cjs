6618889607dc783286b9531bd666c9caa45add9b
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-02 00:30:09 +0300

    chore: Exclude tailwind and postcss config from being linted

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "tailwind.config.js", "postcss.config.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
