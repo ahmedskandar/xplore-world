a45791b95f66367d391bcca1e9cb217e58a50538
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-02 20:56:46 +0300

    feat: Landing content complete

--- src/pages/home/LandingContent.tsx
+++ src/pages/home/LandingContent.tsx
@@ -1,12 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import LinkButton from "../../components/ui/LinkButton";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+
 const LandingContent = () => {
   return (
-    <main className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center text-white">
-      <h1>Map Your Journey, Treasure Your Memories.</h1>
-      <p>
-        Track every step of your adventures with precision, relive your
-        experiences, and safeguard your travel memories for a lifetime.
-      </p>
-      <button>Start Tracking</button>
+    <main className="absolute flex w-full justify-center">
+      <div className="mx-5 mt-8 flex w-full flex-col items-center justify-center rounded-lg bg-black/80 p-6 text-center sm:w-4/5 sm:p-10 lg:w-3/5 lg:px-5 lg:py-14">
+        <h1 className="bg-gradient-primary bg-clip-text pb-2 text-2xl font-bold text-transparent sm:text-3xl md:text-4xl xl:text-5xl">
+          MAP Your Journey, <br />
+          TREASURE Your Memories.
+        </h1>
+        <p className="mb-14 text-white md:text-lg lg:text-xl">
+          Track every step of your adventures with precision, relive your
+          experiences, and safeguard your travel memories for a lifetime.
+        </p>
+        <LinkButton to={"/login"}>
+          <span className="hover-effect">Start Tracking</span>
+          <FontAwesomeIcon icon={faLocationDot} className="hover-effect" />
+        </LinkButton>
+      </div>
     </main>
   );
 };
