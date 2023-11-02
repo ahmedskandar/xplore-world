3820599c03d24077045c160912abde9a124852ad
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-02 14:52:54 +0300

    style: Add classname

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -3,7 +3,11 @@ import { LinkType, ChildrenPropsType } from "../../lib/types";
 import { Link } from "react-router-dom";
 
 const LinkButton = ({ to, children }: LinkType & ChildrenPropsType) => {
-  return <Link to={to}>{children}</Link>;
+  return (
+    <Link className={"bg-gradient-primary"} to={to}>
+      {children}
+    </Link>
+  );
 };
 
 export default LinkButton;
