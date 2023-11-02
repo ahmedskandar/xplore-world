69234024a133a71de563d17b8c749a9acb6e6120
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-02 19:45:44 +0300

    feat: Implement Nav component for navigation

--- src/pages/home/Nav.tsx
+++ src/pages/home/Nav.tsx
@@ -1,10 +1,18 @@
 import { NavLink } from "react-router-dom";
+import LinkButton from "../../components/ui/LinkButton";
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons/faPowerOff";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Nav = () => {
   return (
     <nav className="mt-5 space-x-10">
-      <NavLink to={'login'}>Sign up</NavLink>
-      <NavLink to={'signup'}>Login</NavLink>
+      <NavLink className="text-white hover:font-semibold" to={"signup"}>
+        Sign up
+      </NavLink>
+      <LinkButton to={"login"}>
+        <span className="hover-effect">Login</span>
+        <FontAwesomeIcon className="hover-effect" icon={faPowerOff} />
+      </LinkButton>
     </nav>
   );
 }
