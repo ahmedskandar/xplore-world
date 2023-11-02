2e7281f8d7d4cf453ef77aa16125fba15cd5ba09
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-02 15:20:53 +0300

    style: Add primary button style

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const LinkButton = ({ to, children }: LinkType & ChildrenPropsType) => {
   return (
-    <Link className={"bg-gradient-primary"} to={to}>
+    <Link className={"button-primary"} to={to}>
       {children}
     </Link>
   );
