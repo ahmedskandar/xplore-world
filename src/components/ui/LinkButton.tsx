8f9897b17f96a481ea84a97b2038efca91ae8db4
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-02 17:53:23 +0300

    style: Add group and space x class

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const LinkButton = ({ to, children }: LinkType & ChildrenPropsType) => {
   return (
-    <Link className={"button-primary"} to={to}>
+    <Link className={"button-primary group space-x-2"} to={to}>
       {children}
     </Link>
   );
