0ab0da2990dae257ba51324a99d5cfb71baf6e7c
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-03 11:57:45 +0300

    feat: Implement reusable Input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,18 @@
+import { InputType } from "../../lib/types";
+
+const Input = ({ label, onChange }: InputType) => {
+  //Stops app execution if onChange is not defined. This check is not done in TypeScript to make the
+  //type reusable as possible
+  if (!onChange) throw new Error("The onChange function should be defined");
+  return (
+    <div className="space-y-2">
+      <label htmlFor="input">{label}</label>
+      <input
+        onChange={onChange}
+        className="w-full rounded-sm border border-[#cecece] p-2 focus:shadow-md focus:outline-[#2ECC71]"
+      />
+    </div>
+  );
+};
+
+export default Input;
