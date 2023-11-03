2582a914e98f0d8e5dc450334894fec56d94850e
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-03 08:44:39 +0300

    style: Add accent color to checked input

--- src/pages/login/LoginOptions.tsx
+++ src/pages/login/LoginOptions.tsx
@@ -9,15 +9,16 @@ const LoginOptions = () => {
      };
   return (
     <div className="flex justify-between">
-      <label className="flex gap-1 items-center">
+      <label className="flex items-center gap-1">
         <input
           type="checkbox"
+          className="accent-[#009ACD]"
           checked={isChecked}
           onChange={handleCheckboxChange}
         />
         <span className="cursor-pointer">Remember me</span>
       </label>
-      <Link to={'/reset'}>Forgot password?</Link>
+      <Link to={"/reset"}>Forgot password?</Link>
     </div>
   );
 }
