e46b23d3a6977e1a66bb549a0424aea8b605fa9c
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-03 15:51:28 +0300

    Add: Input type for flexibility

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,6 +1,6 @@
 import { InputType } from "../../lib/types";
 
-const Input = ({ label, onChange }: InputType) => {
+const Input = ({ label, onChange, type }: InputType) => {
   //Stops app execution if onChange is not defined. This check is not done in TypeScript to make the
   //type reusable as possible
   if (!onChange) throw new Error("The onChange function should be defined");
@@ -8,6 +8,7 @@ const Input = ({ label, onChange }: InputType) => {
     <div className="space-y-2">
       <label htmlFor="input">{label}</label>
       <input
+      type={type}
         onChange={onChange}
         className="w-full rounded-sm border border-[#cecece] p-2 focus:shadow-md focus:outline-none focus:border-[#2ECC71]"
       />
