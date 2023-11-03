2cbe562aae1b86218eeaa63183560ccde959afca
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-03 16:49:14 +0300

    feat: Implement LoginIllustration component

---
+++ src/pages/login/LoginIllustration.tsx
@@ -0,0 +1,9 @@
+const LoginIllustration = () => {
+  return (
+    <div className="order-1 md:order-2 md:basis-1/2">
+      <img className="md:w-full md:h-full" src="/assets/images/plane.jpg" alt="Eye catching flying aeroplane wing image" />
+    </div>
+  );
+};
+
+export default LoginIllustration;
