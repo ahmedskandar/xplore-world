19910d1f7a8e97cad0ce2692f278786f97e11853
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-03 16:52:20 +0300

    add: Add Login page to app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="login" element={<></>} />
+          <Route path="login" element={<Login />} />
           <Route path="signup" element={<></>} />
           <Route path="app" element={<></>}>
             <Route index element={<Navigate to={"travels"} replace />} />
