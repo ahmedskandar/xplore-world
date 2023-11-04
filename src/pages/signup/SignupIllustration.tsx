65706c35412d3607b21cd38f7e009343c9c381b4
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-04 16:10:14 +0300

    feat: Implement SignUpIllustration component

---
+++ src/pages/signup/SignupIllustration.tsx
@@ -0,0 +1,13 @@
+const SignupIllustration = () => {
+  return (
+    <div className="md:basis-1/2">
+      <img
+        className="rounded-r-full object-cover object-bottom md:h-full md:w-full"
+        src="/assets/images/boat-travel.jpg"
+        alt="People on a boat, enjoying a river amid houses."
+      />
+    </div>
+  );
+};
+
+export default SignupIllustration;
