719d7d3942e36057703328e39f43ecc6f656884c
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-05 12:20:33 +0300

    feat: Add authentication context provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="app" element={<></>}>
-            <Route index element={<Navigate to={"travels"} replace />} />
-            <Route path="form" element={<></>} />
-            <Route path="travels" element={<></>} />
-          </Route>
-          <Route path="*" element={<></>} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="app" element={<></>}>
+              <Route index element={<Navigate to={"travels"} replace />} />
+              <Route path="form" element={<></>} />
+              <Route path="travels" element={<></>} />
+            </Route>
+            <Route path="*" element={<></>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
