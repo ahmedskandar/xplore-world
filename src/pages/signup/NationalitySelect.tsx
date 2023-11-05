300ab2a3b0ab8101ed9c1381f768d31813764faf
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-05 10:05:38 +0300

    feat: Implement nationality select input

---
+++ src/pages/signup/NationalitySelect.tsx
@@ -0,0 +1,24 @@
+import { NationalitySelect } from "../../lib/types";
+
+const NationalitySelect = ({ onChange, select, error }: NationalitySelect) => {
+    if (!onChange) throw new Error("The onChange function should be defined");
+  return (
+    <div className="space-y-2">
+      <label htmlFor="">Select your nationality:</label>
+      <select
+        onChange={onChange}
+        className={`input ${
+          error && "border-red-500"
+        }  focus:border-[#2ECC71] focus:shadow-md focus:outline-none`}
+        value={select}
+      >
+        <option value="" disabled className="text-gray-400"></option>
+        <option value="CA">Canada</option>
+        <option value="KE">Kenya</option>
+        <option value="EG">Egypt</option>
+      </select>
+    </div>
+  );
+};
+
+export default NationalitySelect;
