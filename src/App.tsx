16544ee1707541795e2527a9aa88a02426b1cfab
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-05 10:20:18 +0300

    feat: Implement SignUp page and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="login" element={<Login />} />
-          <Route path="signup" element={<></>} />
+          <Route path="signup" element={<Signup />} />
           <Route path="app" element={<></>}>
             <Route index element={<Navigate to={"travels"} replace />} />
             <Route path="form" element={<></>} />
---
+++ src/pages/signup/Signup.tsx
@@ -0,0 +1,14 @@
+import PageLayout from "../../components/ui/PageLayout"
+import SignupForm from "./SignupForm"
+import SignupIllustration from "./SignupIllustration"
+
+const Signup = () => {
+  return (
+    <PageLayout>
+        <SignupIllustration />
+        <SignupForm />
+    </PageLayout>
+  )
+}
+
+export default Signup
