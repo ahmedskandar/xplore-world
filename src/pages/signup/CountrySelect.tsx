fa3d78bc9479adb583d168f7ea1ee1e2e54a3129
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-06 14:44:47 +0300

    feat: Implement country data fetch

--- src/pages/signup/CountrySelect.tsx
+++ src/pages/signup/CountrySelect.tsx
@@ -1,7 +1,38 @@
-import { NationalitySelect } from "../../lib/types";
+import { useEffect, useState } from "react";
+import { CountryDataType, NationalitySelect } from "../../lib/types";
+import { apiKey } from "../../data/constants";
 
 const CountrySelect = ({ onChange, select, error }: NationalitySelect) => {
-    if (!onChange) throw new Error("The onChange function should be defined");
+  if (!onChange) throw new Error("The onChange function should be defined");
+
+  const [countries, setCountries] = useState<CountryDataType>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "https://referential.p.rapidapi.com/v1/country";
+      const headers = new Headers();
+      headers.append("X-RapidAPI-Key", apiKey ?? "");
+      headers.append("X-RapidAPI-Host", "referential.p.rapidapi.com");
+
+      try {
+        const response = await fetch(url, { method: "GET", headers });
+        if (!response.ok) {
+          throw new Error("Request failed");
+        }
+        const data = (await response.json()) as CountryDataType;
+        setCountries(data);
+      } catch (e) {
+        if (e instanceof Error) {
+          throw new Error(e.message);
+        } else {
+          throw e; // re-throw the error if it's not an instance of Error
+        }
+      }
+    };
+
+    void fetchData();
+  }, []);
+
   return (
     <div className="space-y-2">
       <label htmlFor="">Select your nationality:</label>
@@ -13,9 +44,13 @@ const CountrySelect = ({ onChange, select, error }: NationalitySelect) => {
         value={select}
       >
         <option value="" disabled className="text-gray-400"></option>
-        <option value="CA">Canada</option>
-        <option value="KE">Kenya</option>
-        <option value="EG">Egypt</option>
+        {countries
+          .sort((a, b) => a.value.localeCompare(b.value))
+          .map((country) => (
+            <option key={country.value} value={country.key}>
+              {country.value}
+            </option>
+          ))}
       </select>
     </div>
   );
