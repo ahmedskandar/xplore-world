7234cce49d2e2ae45fc1d8ead7ce3c91ad2b0212
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-09 08:53:06 +0300

    feat: Manage error and loading state and refactor

--- src/pages/signup/CountrySelect.tsx
+++ src/pages/signup/CountrySelect.tsx
@@ -1,16 +1,13 @@
-import { useState, useCallback, useMemo } from "react";
+import { useMemo } from "react";
 import { CountryDataType, NationalitySelect } from "../../lib/types";
 import { apiKey } from "../../data/constants";
 import { useFetch } from "../../hooks/useFetch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import Error from "../../components/ui/ErrorText";
 
 const CountrySelect = ({ onChange, select, error }: NationalitySelect) => {
   if (!onChange) throw new Error("The onChange function should be defined");
-
-  const [countries, setCountries] = useState<CountryDataType>([]);
-
-  const handleCountryChange = useCallback((countriesData: CountryDataType) => {
-    setCountries(countriesData);
-  }, []);
 
   const url = "https://referential.p.rapidapi.com/v1/country";
   const headers = useMemo(() => {
@@ -19,27 +16,40 @@ const CountrySelect = ({ onChange, select, error }: NationalitySelect) => {
       "X-RapidAPI-Host": "referential.p.rapidapi.com",
     });
   }, []);
-  useFetch(url, headers, handleCountryChange);
+
+  const {
+    data: countries,
+    error: fetchError,
+    isLoading,
+  } = useFetch<CountryDataType>(url, headers);
 
   return (
     <div className="space-y-2">
       <label htmlFor="">Select your nationality:</label>
-      <select
-        onChange={onChange}
-        className={`input ${
-          error && "border-red-500"
-        }  focus:border-[#2ECC71] focus:shadow-md focus:outline-none`}
-        value={select}
-      >
-        <option value="" disabled className="text-gray-400"></option>
-        {countries
-          .sort((a, b) => a.value.localeCompare(b.value))
-          .map((country) => (
-            <option key={country.value} value={country.key}>
-              {country.value}
-            </option>
-          ))}
-      </select>
+      {fetchError && <Error>{fetchError}</Error>}
+      {isLoading && (
+        <div className="flex justify-center">
+          <FontAwesomeIcon className="animate-spin" icon={faSpinner} />
+        </div>
+      )}
+      {!isLoading && !fetchError && (
+        <select
+          onChange={onChange}
+          className={`input ${
+            error && "border-red-500"
+          }  focus:border-[#2ECC71] focus:shadow-md focus:outline-none`}
+          value={select}
+        >
+          <option value="" disabled className="text-gray-400"></option>
+          {countries
+            ?.sort((a, b) => a.value.localeCompare(b.value))
+            .map((country) => (
+              <option key={country.value} value={country.key}>
+                {country.value}
+              </option>
+            ))}
+        </select>
+      )}
     </div>
   );
 };
