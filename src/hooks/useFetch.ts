2a2cc0c931856b06fa14f896fca8f9e30dfc2ea7
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-09 09:13:37 +0300

    feat: Add empty data validation and return stmt

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -5,6 +5,19 @@ export const useFetch = <T>(url: string, headers: Headers) => {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
+  function isDataEmpty(data: unknown): boolean {
+    //Return true if data is a string or an array of length 0
+    if (typeof data === "string" || Array.isArray(data)) {
+      return data.length === 0;
+    }
+    //Return true if data, which is an object, is null or the object properties have no value
+    if (typeof data === "object") {
+      return data === null || Object.keys(data).length === 0;
+    }
+    //Return true if data is falsy in general
+    return !data;
+  }
+
   useEffect(() => {
     const controller = new AbortController();
     const fetchData = async () => {
@@ -16,9 +29,12 @@ export const useFetch = <T>(url: string, headers: Headers) => {
           signal: controller.signal,
         });
         if (!response.ok) {
-          setError("Request failed")
+          return setError("Request failed");
         }
         const data = (await response.json()) as T;
+        if (isDataEmpty(data)) {
+         return setError("Empty data");
+        }
         setData(data);
       } catch (e) {
         if (e instanceof Error && e.name !== "AbortError") setError(e.message);
@@ -28,7 +44,7 @@ export const useFetch = <T>(url: string, headers: Headers) => {
     };
     void fetchData();
 
-    return () => controller.abort()
+    return () => controller.abort();
   }, [headers, url]);
 
   return { data, error, isLoading };
