10b5df417a80444ca22aea6eda2523ec7b193678
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-16 05:09:25 +0300

    fix: country not showing

--- src/pages/signup/CountrySelect.tsx
+++ src/pages/signup/CountrySelect.tsx
@@ -1,13 +1,16 @@
-import { useMemo } from "react";
-import { CountryDataType, NationalitySelect } from "../../lib/types";
+import { useEffect, useMemo, useState } from "react";
+import { NationalitySelect } from "../../lib/types";
 import { apiKey } from "../../data/constants";
-import { useFetch } from "../../hooks/useFetch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import ErrorText from "../../components/ui/ErrorText";
 
 const CountrySelect = ({ onChange, select, error }: NationalitySelect) => {
   if (!onChange) throw new Error("The onChange function should be defined");
+  const [isLoading, setIsLoading] = useState(true);
+  const [countries, setCountries] = useState<{
+    value: string;
+    key: string;
+  }[] | null>(null);
 
   const url = "https://referential.p.rapidapi.com/v1/country";
   const headers = useMemo(() => {
@@ -17,24 +20,43 @@ const CountrySelect = ({ onChange, select, error }: NationalitySelect) => {
     });
   }, []);
 
-  const {
-    data: countries,
-    error: fetchError,
-    isLoading,
-  } = useFetch<CountryDataType>(url, headers);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, {
+          headers,
+        });
+        //eslint-disable-next-line
+        const data: { value: string; key: string }[] = await response.json();
+        setCountries(data);
+
+        //eslint-disable-next-line
+      } catch (e) {
+        // if (e instanceof Error && e.name !== "AbortError") setError(e.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    void fetchData();
+  }, [headers]);
+
+  // const {
+  //   data: countries,
+  //   error: fetchError,
+  //   isLoading,
+  // } = useFetch<CountryDataType>(url, headers);
 
   return (
     <div className="space-y-2">
       <label htmlFor="select">Select your nationality:</label>
-      {fetchError && <ErrorText>{fetchError}</ErrorText>}
       {isLoading && (
         <div className="flex justify-center">
           <FontAwesomeIcon className="animate-spin" icon={faSpinner} />
         </div>
       )}
-      {!isLoading && !fetchError && (
+      {!isLoading && (
         <select
-        id="select"
+          id="select"
           onChange={onChange}
           className={`input ${
             error && "border-red-500"
@@ -42,8 +64,7 @@ const CountrySelect = ({ onChange, select, error }: NationalitySelect) => {
           value={select}
         >
           <option value="" disabled className="text-gray-400"></option>
-          {countries
-            ?.sort((a, b) => a.value.localeCompare(b.value))
+          {countries?.sort((a, b) => a.value.localeCompare(b.value))
             .map((country) => (
               <option key={country.value} value={country.key}>
                 {country.value}
